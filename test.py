c4be5e202177e84d0fc05b5d2a16cbbadf9042ab
Matthew Hawkins <matthew.hawkins1115@gmail.com>
2021-06-06 00:51:57 -0700

    Fixed linting/func name errors

--- test.py
+++ test.py
@@ -6,8 +6,9 @@ class Testcase(unittest.TestCase):
 
     def test_add_1(self):
         self.assertEqual(example.addition(1, 2), 3)
-    def test_add_1(self):
-        self.assertEqual(example.subtraction(1, 0), 1)
+
+    def test_subtract_1(self):
+        self.assertEqual(example.subtract(1, 0), 1)
 
 
 if __name__ == '__main__':
