623d915f8115daa30503a6b257253da986824484
Matthew Hawkins <matthew.hawkins1115@gmail.com>
2021-06-06 00:50:07 -0700

    Added subtraction to fix test

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class Testcase(unittest.TestCase):
     def test_add_1(self):
         self.assertEqual(example.addition(1, 2), 3)
     def test_add_1(self):
-        self.assertEqual(example.subtract(1, 0), 1)
+        self.assertEqual(example.subtraction(1, 0), 1)
 
 
 if __name__ == '__main__':
