c4ab8a8699f1e1a2124de220a774bc545ac487a4
Matthew Hawkins <matthew.hawkins1115@gmail.com>
2021-06-06 00:56:49 -0700

    Added final tests

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ class Testcase(unittest.TestCase):
     def test_subtract_1(self):
         self.assertEqual(example.subtract(1, 0), 1)
 
+    def test_mult_1(self):
+        self.assertEqual(example.multiply(1, 0), 0)
+
+    def test_div_1(self):
+        self.assertEqual(example.divide(1, 1), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
